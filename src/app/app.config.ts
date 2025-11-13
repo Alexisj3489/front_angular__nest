fed82887335059c8075c746dacc90a3367279c86
Alexisj3489 <ajg.coronel@yavirac.edu.ec>
2025-11-12 23:30:39 -0500

    ultimos cambios finales

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { BrowserModule, platformBrowser, provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()),importProvidersFrom(BrowserModule),importProvidersFrom(BrowserAnimationsModule)]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())]
 };
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private baseUrl="http://127.0.0.1:3000"
-  private http=inject(HttpClient)
+  private baseUrl = environment.urlServidor
+  private http = inject(HttpClient)
 
   constructor() {}
+  
   loginConNest(credenciales: any){
-    return this.http.post<any>(`${this.baseUrl}/auth/login`,credenciales);
+    return this.http.post<any>(`${this.baseUrl}/auth/login`, credenciales);
   }
 
   registroConNest(datos:any){
-    return this.http.post<any>(`${this.baseUrl}/register`,datos);
+    // ¡CORRECCIÓN AQUÍ! Debe incluir /auth/register
+    return this.http.post<any>(`${this.baseUrl}/auth/register`, datos);
   }
 }
